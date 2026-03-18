16b2a26c60c463fcdf9a51cb75b24a25fb122ec3
jestudillo <jestudillo@ucc.mx>
2026-03-17 19:20:30 -0600

    Conexión a MongoDb

---
+++ BackFrontEndMongoDB/models/Usuario.js
@@ -0,0 +1,22 @@
+const mongoose = require("mongoose");
+
+const UsuarioEsquema = new mongoose.Schema({
+    nombre: {
+        type: String,
+        required: true
+    },
+    email: {
+        type: String,
+        required: true
+    },
+
+    genero: {
+        type: String,
+        required:true
+    },
+    plataformas:{
+        type: [String]
+    }
+});
+
+module.exports = mongoose.model("Usuario", UsuarioEsquema);
--- BackFrontEndMongoDB/server.js
+++ BackFrontEndMongoDB/server.js
@@ -1,5 +1,8 @@
 const express=require("express");
-const cors=require("cors");
+const cors=require("cors"); 
+const mongoose = require('mongoose');
+
+const Usuario = require("./models/Usuario");
 
 const app=express();
 const port=3600;
@@ -8,41 +11,32 @@ app.use(cors());
 app.use(express.json());
 app.use(express.static("public"));
 
-let registros = [
-    {
-        id:1,
-        nombre: "Ana López",
-        email: "ana@ficachi.mx",
-        genero: "Femenino",
-        plataformas: ["Netflix", "HBO"]
-    },
-    
-    {
-        id:2,
-        nombre: "Juan González",
-        email: "juan@gmail.com",
-        genero: "Masculino",
-        plataformas: ["HBO"]
-    }
-];
+mongoose.connect("mongodb://sergioac:Chechorrol@ac-cgbyvyp-shard-00-00.l5sli8p.mongodb.net:27017,ac-cgbyvyp-shard-00-01.l5sli8p.mongodb.net:27017,ac-cgbyvyp-shard-00-02.l5sli8p.mongodb.net:27017/?db=GrupoB&ssl=true&replicaSet=atlas-as79dy-shard-0&authSource=admin&appName=Cluster0")
+.then(
+    ()=>console.log("MongoDB database service listo")
+)
+.catch(
+    err => console.log(err)
+);
 
-let idActual = 3;
 
-app.get("/api/usuarios", (req, res)=>{
-    res.json(registros);
+app.get("/api/usuarios", async(req, res)=>{
+    const usuarios = await Usuario.find();
+    res.json(usuarios);
 });
 
-app.post("/api/usuarios", (req, res)=> {
-    const nuevo = {
-        id: idActual++,
-        nombre: req.body.nombre,
-        email: req.body.email,
-        genero: req.body.genero,
-        plataformas: req.body.plataformas
-    };
-
-    registros.push(nuevo);
-    res.json(nuevo);
+app.post("/api/usuarios", async(req, res)=> {
+    const nuevo = new Usuario(
+        {
+            nombre: req.body.nombre,
+            email: req.body.email,
+            genero: req.body.genero,
+            plataformas: req.body.plataformas
+        }
+    );
+    
+    const guardado = await nuevo.save();
+    res.json(guardado);
 });
 
 app.put("/api/usuarios/:id", (req, res)=>{
